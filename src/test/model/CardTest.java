e7230dd3597b1710e705549c35cf6465df893bc2
Lana <lnakai@students.cs.ubc.ca>
2026-02-04 12:38:41 -0800

    Start making tests for Cards

--- src/test/model/CardTest.java
+++ src/test/model/CardTest.java
@@ -1,19 +1,28 @@
 package model;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CardTest {
+
+    Card c1;
     
     @BeforeEach
     void runBefore() {
-
+        c1 = new Card("A1", "A", "");
     }
 
     @Test
-    void sampleTest() {
-        assertTrue(true);
+    void testConstructor() {
+        assertEquals("A1", c1.getActivity());
+        assertEquals("A", c1.getDeckName());
+        assertEquals("", c1.getDescription());
+    }
+    @Test
+    void testAddCardToDecks(){
+        
     }
 }
