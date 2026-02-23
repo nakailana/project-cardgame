e241f242fa1c01e83432a95d241a9cc4ec0a965e
Lana <lnakai@students.cs.ubc.ca>
2026-02-23 15:47:32 -0800

    Implement DecksController

--- src/main/model/DecksController.java
+++ src/main/model/DecksController.java
@@ -6,23 +6,27 @@ import java.util.List;
 // Represents all the decks of cards in the program
 public class DecksController {
 
+    private List<Deck> decks;
+
     /*
      * EFFECTS: create a new instance of the game with an empty
      * ArrayList of decks
      */
     public DecksController() {
-        // stub
+        decks = new ArrayList<Deck>();
     }
 
     // MODIFIES: this
     // EFFECTS: adds deck to the deck controller if not already added
     public void addToController(Deck deck) {
-        // stub
+        if (!decks.contains(deck)) {
+            decks.add(deck);
+        }
     }
 
     // getters
     public List<Deck> getDecks() {
-        return null; //stub
+        return decks;
     }
 
 }
