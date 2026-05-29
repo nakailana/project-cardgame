703dccfc72dc2dec7cfe27225bce1c7e8ca23376
Lana <koalanako@gmail.com>
2026-05-29 15:04:55 -0700

    api.js stubs

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,31 @@
+//Helper file for all fetch calls
+
 const BASE_URL = "http://localhost:8080/api";
+
+export const getDecks = async () => {
+  
+};
+
+export const createDeck = async (name) => {
+  
+};
+
+export const deleteDeck = async (id) => {
+
+};
+
+export const getCards = async (deckId) => {
+  
+};
+
+export const addCard = async (deckId, card) => {
+  
+};
+
+export const deleteCard = async (deckId, cardId) => {
+  
+};
+
+export const drawCard = async (deckId, outdoor = null) => {
+  
+};
