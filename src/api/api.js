f3d40f54f912a84c95fccc739a64705e68389b32
Lana <koalanako@gmail.com>
2026-07-24 11:23:01 -0700

    finish api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,29 +3,48 @@
 const BASE_URL = "http://localhost:8080/api";
 
 export const getDecks = async () => {
-  
+  const res = await fetch(`${BASE_URL}/decks`);
+  return res.json();
 };
 
 export const createDeck = async (name) => {
-  
+  const res = await fetch(`${BASE_URL}/decks`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name }),
+  });
+  return res.json();
 };
 
 export const deleteDeck = async (id) => {
-
+    await fetch(`${BASE_URL}/decks/${id}`, { method: "DELETE" });
 };
 
 export const getCards = async (deckId) => {
-  
+  const res = await fetch(`${BASE_URL}/decks/${deckId}/cards`);
+  return res.json();
 };
 
 export const addCard = async (deckId, card) => {
-  
+  const res = await fetch(`${BASE_URL}/decks/${deckId}/cards`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(card),
+  });
+  return res.json();
 };
 
 export const deleteCard = async (deckId, cardId) => {
-  
+  await fetch(`${BASE_URL}/decks/${deckId}/cards/${cardId}`, {
+    method: "DELETE",
+  });
 };
 
 export const drawCard = async (deckId, outdoor = null) => {
-  
+  const url = outdoor !== null
+    ? `${BASE_URL}/decks/${deckId}/draw?outdoor=${outdoor}`
+    : `${BASE_URL}/decks/${deckId}/draw`;
+  const res = await fetch(url);
+  if (res.status === 404) return null;
+  return res.json();
 };
